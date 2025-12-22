057493a0736fefae7d70928a3583e15cecee4819
j4v1r <jcolungaa2000@alumno.ipn.mx>
2025-12-22 14:28:02 -0600

    Instruc Programa

---
+++ CompiladoresHoc5/src/CompiladoresHoc5/InstrucPrograma.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CompiladoresHoc5;
+
+/**
+ *
+ * @author Rogelio Colunga R
+ */
+public class InstrucPrograma {
+    
+    public EnumTipoInstr TipInstr; //INSTRUC, SYMBOLM BLTIN
+    public EnumInstrMaq Instruc;   /*EVAL, ADD, SUB, ..., ASSING, BLTIN, 
+                                   VARPUSH, PRINT, STOP*/
+    public EnumBLTIN Func_BLTIN;
+    public SymbolHoc symbolHoc;
+    
+}
